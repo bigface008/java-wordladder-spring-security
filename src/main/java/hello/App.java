bb04b6f34163676254d08e96c787237f30110cbc
bigface008 <bigface00@sjtu.edu.cn>
2018-04-29 15:18:55 +0800

    Simple security authentication

---
+++ src/main/java/hello/App.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+/**
+ * App of WordLadder
+ * @author 516030910460
+ * @version 0.0.2
+ */
+
+@RestController
+public class App {
+
+    @RequestMapping(value = "/app",method = RequestMethod.GET)
+    public String say(String[] args) throws IOException {
+            WordLadder map = new WordLadder("dictionary.txt", 3);
+            return map.ladder("dog", "cat");
+    }
+}
---
+++ src/main/java/hello/WordLadder.java
@@ -0,0 +1,158 @@
+package hello;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * WordMap
+ * @author 516030910460
+ * @version 0.0.1
+ */
+
+public class WordLadder {
+    private Set<String> dictionary = null; // Record for all words.
+    private ArrayList<String> path = null; // Path recording the final result.
+    private Map<String, String> map = null; // Map used for storing the path.
+    private Queue<String> queue = null; // Queue used in BFS.
+
+    public WordLadder(String file_name, int len) throws IOException {
+        File dic = new File(file_name);
+        FileInputStream fin = new FileInputStream(dic);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fin));
+
+        String line = null;
+        dictionary = new HashSet<>();
+        while ((line = br.readLine()) != null)
+            if (line.length() == len) // Only add words with the given length into the dictionary.
+                dictionary.add(line);
+
+        br.close();
+    }
+
+    /**
+     * ladder
+     * The core function for finding paths from word_2 to word_1 with BFS.
+     */
+
+    public String ladder(String from, String to) {
+        // Change to lower case.
+        String word_1 = from.toLowerCase();
+        String word_2 = to.toLowerCase();
+
+        // Check for wrong input.
+        checkWords(word_1, word_2);
+
+        // BFS
+        queue = new LinkedList<>();
+        map = new HashMap<>();
+        queue.add(word_1);
+        dictionary.remove(word_1);
+        while (!queue.isEmpty()) {
+            String temp = queue.poll();
+            ArrayList<String> neighbors = findNeighbors(temp);
+            for (String n : neighbors) {
+                map.put(n, temp);
+                if (n.equals(word_2)) { // Check if get the final word.
+                    return "Paths: " + findPath(word_1, word_2);// Print the result.
+                }
+                dictionary.remove(n); // Ensure to remove the used word from the dictionary.
+                queue.add(n);
+            }
+        }
+        // No path avalible.
+        return "No such path between " + word_1 + " and " + word_2 + ".";
+    }
+
+    /**
+     * getStepNumber
+     * Get the number of words in path
+     */
+
+    public int getStepNumber() {
+        return path.size();
+    }
+
+    /**
+     * checkWords
+     * Check if the requirements listed below are satisfied.
+     */
+
+    private void checkWords(String word_1, String word_2) {
+        if (word_1.length() != word_2.length()) { // Word must be the same length.
+            System.out.println("The words must be the same length.");
+            System.exit(1);
+        } else if (word_1.equals(word_2)) { // Word shouldn't be the same.
+            System.out.println("The words must be different.");
+            System.exit(1);
+        } else if (!dictionary.contains(word_1) || !dictionary.contains(word_2)) { // Words must exist in the dictionary.
+            System.out.println("Make sure to input right words.");
+            System.exit(1);
+        }
+    }
+
+    /**
+     * testCheckWords
+     * CheckWords() used for testing.
+     */
+
+    public boolean testCheckWords(String word_1, String word_2) {
+        if (word_1.length() != word_2.length()) { // Word must be the same length.
+            return false;
+        } else if (word_1.equals(word_2)) { // Word shouldn't be the same.
+            return false;
+        } else if (!dictionary.contains(word_1) || !dictionary.contains(word_2)) { // Words must exist in the dictionary.
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * findNeighbors
+     * Find neighbors for word in the dictionary.
+     */
+
+    private ArrayList<String> findNeighbors(String word) {
+        ArrayList<String> neighbors = new ArrayList<>();
+        for (String str : dictionary) {
+            int len = word.length();
+            int check = 0; // Record the number of different char in one word.
+            for (int i = 0; i < len; i++) {
+                if (word.charAt(i) != str.charAt(i))
+                    check++;
+                if (check > 1) // If str is not the right word.
+                    break;
+            }
+            if (check == 1)
+                neighbors.add(str);
+        }
+        return neighbors;
+    }
+
+    /**
+     * findPath
+     * Build the path with the record sorted in map.
+     */
+
+    private String findPath(String word_1, String word_2) {
+        path = new ArrayList<>();
+        path.add(word_2);
+
+        // Get word from map
+        String temp = map.get(word_2);
+        while (!temp.equals(word_1)) {
+            path.add(temp);
+            temp = map.get(temp);
+        }
+
+        String result = "";
+        for (String word : path) {
+            result += (word + " ");
+        }
+        result += (word_1 + " "); // Don't forget to add word_1.
+        return result;
+    }
+}
